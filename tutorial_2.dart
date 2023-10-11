8ff2c7180186c98ab50d2e06334f180f0f902676
Razzaqdeveloper <104042202+Razzaqdeveloper@users.noreply.github.com>
2023-10-11 00:04:12 -0700

    tutorial_2.dart
    adding comments single line and multiple line comments

--- tutorial_2.dart
+++ tutorial_2.dart
@@ -1,8 +1,26 @@
 void main() {
   
+  
+  /* goood 
+   * 
+   * bb              //multi line comments 
+   * 
+   * dndn
+   * dbdb*/
+  
+  
+  //this is my first line code
   print('hello world');
+  
+  //this is second line code
   print('this is my new dart program');
-  print('my name is abdur razzaq');
-  print(20);
+  
+  
+  print('my name is abdur razzaq'); //this is my name
+  
+  //performing arthematic operation
+  print(20); //this is my age 
+  
+  //printing out boolen value 
   print(true);
 }
