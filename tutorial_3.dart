424056716a2ab0d935f8425299bb82583842625c
Razzaqdeveloper <104042202+Razzaqdeveloper@users.noreply.github.com>
2023-10-11 00:42:03 -0700

    tutorial_3.dart

---
+++ tutorial_3.dart
@@ -0,0 +1,53 @@
+void main() {
+  
+  //this section 3 covers 
+  //    1-How to declare a variables?
+  //    2-What are built-data types in Dart?
+  //    3-What are literals?
+  //    4-String interpolations
+  //    5-Constants in Dart
+  //          a: final keyword
+  //          b: const keyword
+  
+  
+  // Dart has special support for these data types 
+  // 1: Numbers
+  //    a:int
+  //    b:double
+  // 2: Strings
+  // 3: Booleans
+  // NOTE:all data types in dart are objects
+  
+  //define a value of 10 >>> lets say it is your age 
+  //    data types   variable name  value
+  //        int          age      =   10;
+  //        var          age      =   10; //its is inferred as ineger automatically
+  
+  //define a value of "abdurrazzaq" >>> lets say it is your name 
+  //    data types   variable name  value
+  //      string          name     = "abdurrazzaq";
+  //        var           name     = "abdurrazzaq"; 
+  
+  //define a value of true; >>> person is alive = true OR dead = false 
+  //    data types   variable name  value
+  //       bool         isAlive    = true;
+  //        var         isAlive    = true;
+  
+  //number: int
+  int score = 20;
+  
+  //numbers: double
+  double percentage = 97.4;
+  
+  //strings
+  
+  var name = "abdurrazzaq";
+  //boolean
+  bool isValid = true;
+  
+  print(name);
+  print(score);
+  print(percentage);
+  print(isValid);
+  
+}
