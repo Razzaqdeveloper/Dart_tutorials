0959319c12c55a4fb0a52ae996d31b1f79f05f5b
Abdur Razzaq <104042202+Razzaqdeveloper@users.noreply.github.com>
2023-10-11 22:51:31 -0700

    tutorial_5.dart

---
+++ tutorial_5.dart
@@ -0,0 +1,18 @@
+void main() {
+  
+  //final keyword
+  final villagename = 'sarozai';
+//  villagename = 'naryab'; //you can't change the value in final the result will be error
+  final String villagename = 'sarozai'; //we can also write this
+  
+  //const keyword
+  const PI = 3.14;
+  const double gravity = 9.33;
+  
+}
+class circle {
+  
+  final color = 'Red';
+ // const PI = 3.14; // i can't write only const in class level bcz compiler does not happy
+  static const PI = 3.14; // we can use in class level static const 
+}
