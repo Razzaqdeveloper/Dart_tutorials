fca58801ad451e50fee0853b04f1005f4d158530
Abdur Razzaq <104042202+Razzaqdeveloper@users.noreply.github.com>
2023-10-17 09:16:06 -0700

    Switch case Statement

---
+++ tutorial_8.dart
@@ -0,0 +1,71 @@
+void main() {
+  print ("Enter your Grade A , B , C and other.");
+  
+  //switch case allow only 'int' and 'string'
+  // not allow to 'boolean' and 'double' 
+  String grade = "B";
+  
+  switch (grade){
+      
+    case 'A':
+      print ("You are very excelent student");
+      break;
+      
+    case 'B':
+      print ("You are very good student");
+      break;
+      
+    case 'C':
+      print ("You are good student but focus on student");
+      break;
+      
+    default:
+      print ("You are failed sorry");
+      
+  }
+  
+  
+  
+/* 
+   
+     ----- if and else if statement -----
+ 
+if (value 1){
+    execute code here
+}
+else if (value 2){
+    execute code here
+}
+else if (value 3){
+    execute value here
+}
+else {
+    finally execute here
+    if nothing works out 
+}
+
+       ----- switch case statement -----
+  
+  switch(variable){
+      
+      case 1:
+      execute code here 
+      breake;
+      
+      case 2:
+      execute code here 
+      breake;
+      
+      case 3:
+      execute code here 
+      breake;
+      
+      default:
+      finally execute code here
+      
+  }
+    
+   */
+  
+  
+}
