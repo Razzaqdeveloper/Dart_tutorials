1eefbc39b399d78e6d95fde2e76579f4b2d81f45
Abdur Razzaq <104042202+Razzaqdeveloper@users.noreply.github.com>
2023-10-17 21:35:38 -0700

    For , While and Do while loops

---
+++ tutorial_9.dart
@@ -0,0 +1,38 @@
+void main() {
+  
+//   For loop in dart
+     for(var i = 0; i < 10; i++){
+       
+       print("Abdur Razzaq");
+     }
+  
+//   while loop in dart
+//   var i = 0;
+//   while(i < 6){
+    
+//     print("Waqas Ahmad");
+//     i++;
+//   }
+  
+//   do while loop in dart 
+//   var i=0;
+//   do{
+//     print("Haris Khan");
+//     i++;
+    
+//   }
+//   while(i<5);
+  
+  // similarity of loop condition 
+  // counter variable (i=0)
+  // condition check (i<6)
+  // increment/decrement the counter (i++)
+  
+  //differences b/w for,while,do while
+  
+  //   Loop >>>>> difinite --- for loop
+  //   Loop >>>>> indefinite --- while loop and do while loop
+  
+  //  while loop >> we first check the condition
+  //  do while loop >> we check the condition very end of the loop
+}
